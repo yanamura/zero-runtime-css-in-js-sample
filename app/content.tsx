9ea478586cded0cbc7e2d27ecb2a05717ccb0d12
Yasuharu Yanamura <yanamura3@gmail.com>
2024-12-01 18:05:01 +0900

    linaria

--- app/content.tsx
+++ app/content.tsx
@@ -1,9 +1,16 @@
+import { styled } from '@linaria/react';
+
+const SubTitle = styled.h2`
+  font-weight: bold;
+  color: ${(props) => props.color || 'black'};
+`;
+
 export default function Content() {
   return (
     <div>
       <h1>Hello, World!</h1>
-      <h2 style={{ color: "red" }}>red</h2>
-      <h2 style={{ color: "blue" }}>blue</h2>
+      <SubTitle color="red">red</SubTitle>
+      <SubTitle color="blue">blue</SubTitle>
     </div>
   );
 }
