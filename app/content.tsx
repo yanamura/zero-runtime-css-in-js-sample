c37c61b19fa0c1fc264e86903ca6c372abdfa665
Yasuharu Yanamura <yanamura3@gmail.com>
2024-12-01 17:45:03 +0900

    add content.tsx

---
+++ app/content.tsx
@@ -0,0 +1,9 @@
+export default function Content() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <h2 style={{ color: "red" }}>red</h2>
+      <h2 style={{ color: "blue" }}>blue</h2>
+    </div>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import Content from "./content";
 
 export default function App() {
   return (
@@ -17,32 +18,7 @@ export default function App() {
       </head>
       <body>
         <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
+          <Content/>
         </div>
 
         <ScrollRestoration />
