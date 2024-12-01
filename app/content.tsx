35acdd9b22ca9feee60238f8c54a5da0e3771f5f
Yasuharu Yanamura <yanamura3@gmail.com>
2024-12-01 18:26:37 +0900

    KumaUI

--- app/content.tsx
+++ app/content.tsx
@@ -1,9 +1,17 @@
+import { styled } from "@kuma-ui/core";
+
+const SubTitle = styled.h2`
+  font-weight: bold;
+`;
+
+// color: ${(props) => props.color}; // 自前で定義したプロパティは使えない
+
 export default function Content() {
   return (
     <div>
       <h1>Hello, World!</h1>
-      <h2 style={{ color: "red" }}>red</h2>
-      <h2 style={{ color: "blue" }}>blue</h2>
+      <SubTitle color="red">red</SubTitle>{/** colorはKumaUIのUtility Props(https://www.kuma-ui.com/docs/Components/UtilityProps#color) */}
+      <SubTitle color="blue">blue</SubTitle>
     </div>
   );
 }
